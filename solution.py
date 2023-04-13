68757196362cf449049fb411ccb07291530c9979
EnderPortman <103992395+EnderPortman@users.noreply.github.com>
2023-04-13 18:57:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
 
+chat_id = 192196854
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool:    
+    return stats.anderson_ksamp([x, y]).pvalue < 0.04 
